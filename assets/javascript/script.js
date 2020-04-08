997039438b35ce0ea5e3ef1e0fa0747c5a0c9187
Tyler Pho <owner@Tyler-Phos-MacBook-Pro.local>
2020-04-08 16:09:49 -0600

    adding functions to script.js

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -5,3 +5,34 @@ var authKey = "&appid=32e1c02add4067945d7c6604f73cc6cd"
 var queryURL = "api.openweathermap.org/data/2.5/weather?q="
 var listSection = $(".list-group")
 var city = $("#search-city")
+
+var searchedCities = {
+    cities: []
+};
+
+var searchedForecasts = {
+    forecasts: []
+};
+
+$(document).ready(function(){
+    var currentDate = moment().format("L")
+
+    function getSession(){
+        let cities = sessionStorage.getItem("lastOverview")
+        let forecast = sessionStorage.getItem("lastForecast")
+        console.log(cities)
+
+        if(cities) {
+            cities = JSON.parse(cities);
+            searchedCities = cities
+            if(cities.cities.length > 0) {
+                renderWeather(cities.cities[0], true)
+                cities.cities.forEach(function(index){
+                    renderCitiesList(index)
+                })
+            }
+        }
+    }
+
+    
+})
