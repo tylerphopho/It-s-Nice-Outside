6e46aad8846034574d86f874916c81c5a3716b07
Tyler Pho <owner@Tyler-Phos-MacBook-Pro.local>
2020-04-08 15:59:58 -0600

    started making the script.js file

---
+++ assets/javascript/script.js
@@ -0,0 +1,7 @@
+// API Key
+var authKey = "&appid=32e1c02add4067945d7c6604f73cc6cd"
+
+// URL 
+var queryURL = "api.openweathermap.org/data/2.5/weather?q="
+var listSection = $(".list-group")
+var city = $("#search-city")
